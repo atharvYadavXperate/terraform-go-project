ccc223e1f1cb15ce89e39f6207de6682dbbc7245
ravi-burman <ravi.burman@xperate.com>
2026-02-20 21:36:21 +0530

    project initilization and deployment with workflow

---
+++ hello-function/function/go.mod
@@ -0,0 +1,3 @@
+module github.com/atharvYadavXperate/terraform-go-project/helloworld
+
+go 1.25.5
---
+++ hello-function/main.tf
@@ -0,0 +1,77 @@
+variable "project_id" {
+  type  = string
+}
+
+variable "region"{
+    type = string
+}
+
+variable "function_name"{
+    type = string
+}
+
+terraform {
+    required_providers {
+        google = {
+            source  = "hashicorp/google"
+            version = "~> 5.0"
+        }
+        random = {
+            source = "hashicorp/random"
+        }   
+    }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "function_bucket" {
+  name     = "${var.project_id}-${var.function_name}-${random_id.suffix.hex}"
+  location = var.region
+  force_destroy  = true
+  versioning {
+    enabled = true
+  }
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "google_storage_bucket_object" "function_archive" {
+  name   = "function.zip"
+  bucket = google_storage_bucket.function_bucket.name
+  source = "../function.zip"
+}
+
+resource "google_cloudfunctions2_function" "hello_function" {
+  name     = var.function_name
+  location = var.region
+
+  build_config {
+    runtime     = "go122"
+    entry_point = "HelloWorld"
+
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_bucket.name
+        object = google_storage_bucket_object.function_archive.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    ingress_settings   = "ALLOW_ALL"
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "invoker" {
+  location = google_cloudfunctions2_function.hello_function.location
+  service  = google_cloudfunctions2_function.hello_function.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
